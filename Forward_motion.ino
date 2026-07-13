eadc20bc02ed56d83402cef528867410fd609cac
Tabitha <tabitha.paynexx@gmail.com>
2026-07-13 15:25:02 +0100

    Correcting syntax and improving library implementation
    Updated serial baud rate and fixed function declaration.

--- Forward_motion.ino
+++ Forward_motion.ino
@@ -2,6 +2,10 @@
 
 #include <XBOXONE.h>
 
+USB Usb;
+XBOXONE Xbox(&Usb);
+
+
 //motor driver pins
 int in1 = ; //analogue pins
 int in2 = ;
@@ -11,11 +15,19 @@ int en = ; //digital pin
 
 
 void setup() {
-  Serial.begin(9600); //set baud rate
+  Serial.begin(115200); //set baud rate
+
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+    if (Usb.Init() == -1) {
+      Serial.print(F("\r\nOSC did not start"));
+    while (1); //halt
+  }
 
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT); //set pins
   pinMode(en, OUTPUT);
+
+  digitalWrite(en,HIGH);
 
 }
 
@@ -27,13 +39,12 @@ void loop() {
 }
 
 
-void front();{
+void front(){
 
   uint16_t rt = Xbox.getButtonPress(RT); //0-1023, forward
   uint16_t lt = Xbox.getButtonPress(LT); //0-1023, reverse
 
   if (rt > 50) {
-    uint8_t speed = map(rt, 0, 1023, 0, 255);
      digitalWrite(in1, HIGH);
      digitalWrite(in2, LOW); //forward
 
